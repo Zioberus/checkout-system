83232129fc6652c50cbb7f12b7d6bbca73aba727
Zioberus <Daniel.ziobron@icloud.com>
2026-03-21 01:47:12 +0100

    add special price enitity and fix product column constraints

--- src/main/java/com/example/demo/model/SpecialPrice.java
+++ src/main/java/com/example/demo/model/SpecialPrice.java
@@ -1,21 +1,26 @@
 package com.example.demo.model;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+
 @Getter
 @Setter
 @Entity
-public class Product {
+public class SpecialPrice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true, nullable = false
-    )
-    private String item;
-    private BigDecimal normalPrice;
+    @ManyToOne
+    @JoinColumn(name = "productItem", referencedColumnName = "item")
+    private Product product;
+    private int requiredQuantity;
+    private BigDecimal specialPrice;
 
-    public Product(){}
+    public SpecialPrice(){}
+
+
 
 }
